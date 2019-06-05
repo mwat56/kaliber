cf8b3864876e778e1cfda69d389315f51e3fff3f
Matthias Watermann <support@mwat.de>
2019-06-05 09:48:34 +0200

    * initial checkin;

---
+++ app/kaliber.go
@@ -0,0 +1,17 @@
+/*
+   Copyright © 2019 M.Watermann, 10247 Berlin, Germany
+                  All rights reserved
+               EMail : <support@mwat.de>
+*/
+
+package main
+
+import "github.com/mwat56/kaliber"
+
+func main()  {
+	app := kaliber.NewDocument()
+
+
+} // main()
+
+/* _EoF_ */
