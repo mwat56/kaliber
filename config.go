70e20822e0f4a57d0c699a0a1f7f18526ac3e0b3
Matthias Watermann <support@mwat.de>
2019-06-06 21:35:30 +0200

    + completed INI and cmdline handling;

--- config.go
+++ config.go
@@ -9,8 +9,11 @@ package kaliber
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"os/user"
 	"path/filepath"
+	"time"
 
 	"github.com/mwat56/ini"
 )
@@ -76,8 +79,10 @@ func SetCalibreLibraryPath(aPath string) string {
 	if path, err := filepath.Abs(aPath); nil == err {
 		aPath = path
 	}
-	if _, err := os.Stat(aPath); nil == err {
+	if fi, err := os.Stat(aPath); (nil == err) && fi.IsDir() {
 		calibreLibraryPath = aPath
+	} else {
+		calibreLibraryPath = ""
 	}
 
 	return calibreLibraryPath
@@ -90,18 +95,272 @@ func CalibreDatabasePath() string {
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-func initConfig() {
+// `absolute()` return `aDir` as an absolute path
+func absolute(aBaseDir, aDir string) string {
+	if 0 == len(aDir) {
+		return aDir
+	}
+	if '/' == aDir[0] {
+		s, _ := filepath.Abs(aDir)
+		return s
+	}
+
+	return filepath.Join(aBaseDir, aDir)
+} // absolute()
+
+// `iniData()` returns the config values read from INI file(s).
+// The steps here are:
+// (1) read the global `/etc/kaliber.ini`,
+// (2) read the user-local `~/.kaliber.ini`,
+// (3) read the `-ini` commandline argument.
+func iniData() {
+	// (1) ./
+	fName, _ := filepath.Abs("./kaliber.ini")
+	ini1, err := ini.New(fName)
+	if nil == err {
+		ini1.AddSectionKey("", "inifile", fName)
+	}
+
+	// (2) /etc/
+	fName = "/etc/kaliber.ini"
+	if ini2, err := ini.New(fName); nil == err {
+		ini1.Merge(ini2)
+		ini1.AddSectionKey("", "inifile", fName)
+	}
+
+	// (3) ~user/
+	fName = ""
+	if usr, err := user.Current(); nil != err {
+		fName = os.Getenv("HOME")
+	} else {
+		fName = usr.HomeDir
+	}
+	if 0 < len(fName) {
+		fName, _ = filepath.Abs(filepath.Join(fName, ".kaliber.ini"))
+		if ini2, err := ini.New(fName); nil == err {
+			ini1.Merge(ini2)
+			ini1.AddSectionKey("", "inifile", fName)
+		}
+	}
+
+	// (4) cmdline
+	aLen := len(os.Args)
+	for i := 1; i < aLen; i++ {
+		if `-ini` == os.Args[i] {
+			i++
+			if i < aLen {
+				fName, _ = filepath.Abs(os.Args[i])
+				if ini2, _ := ini.New(fName); nil == err {
+					ini1.Merge(ini2)
+					ini1.AddSectionKey("", "inifile", fName)
+				}
+			}
+			break
+		}
+	}
+
+	AppArguments = tAguments{*ini1.GetSection("")}
+} // iniData()
+
+func init() {
+	initArguments()
+} // init()
+
+// `initArguments()` reads the commandline arguments into a list
+// structure merging it with key-value pairs read from an INI file.
+//
+// The steps here are:
+// (1) read the INI file(s),
+// (2) merge the commandline arguments the INI values
+// into the global `AppArguments` variable.
+func initArguments() {
+	iniData()
+
+	bppInt, _ := AppArguments.AsInt("booksperpage")
+	flag.IntVar(&bppInt, "booksperpage", bppInt,
+		"<portNumber> the IP port to listen to ")
+
+	s, _ := AppArguments.Get("datadir")
+	dataStr, _ := filepath.Abs(s)
+	flag.StringVar(&dataStr, "datadir", dataStr,
+		"<dirName> the directory with CSS, IMG, and VIEWS sub-directories\n")
+
+	s, _ = AppArguments.Get("certKey")
+	ckStr := absolute(dataStr, s)
+	flag.StringVar(&ckStr, "certKey", ckStr,
+		"<fileName> the name of the TLS certificate key\n")
+
+	s, _ = AppArguments.Get("certPem")
+	cpStr := absolute(dataStr, s)
+	flag.StringVar(&cpStr, "certPem", cpStr,
+		"<fileName> the name of the TLS certificate PEM\n")
 
 	/*
-		We need:
-		- calibreLibraryPath
-		- calibreDatabaseName
-		- (SQL) LIMIT (rows per page)
-		- View: List _or_ Grid
-
+		s, _ = AppArguments.Get("intl")
+		intlStr := absolute(dataStr, s)
+		flag.StringVar(&intlStr, "intl", intlStr,
+			"<fileName> the path/filename of the localisation file\n")
 	*/
 
-} // initConfig()
+	iniStr, _ := AppArguments.Get("inifile")
+	flag.StringVar(&iniStr, "ini", iniStr,
+		"<fileName> the path/filename of the INI file to use\n")
+
+	langStr, _ := AppArguments.Get("lang")
+	flag.StringVar(&langStr, "lang", langStr,
+		"(optional) the default language to use ")
+
+	lnStr, _ := AppArguments.Get("libraryname")
+	flag.StringVar(&lnStr, "libraryname", lnStr,
+		"Name of this Library (shown on every page)\n")
+
+	lpStr, _ := AppArguments.Get("librarypath")
+	flag.StringVar(&lpStr, "librarypath", lpStr,
+		"Path name of/to the Calibre library\n")
+
+	listenStr, _ := AppArguments.Get("listen")
+	flag.StringVar(&listenStr, "listen", listenStr,
+		"the host's IP to listen at ")
+
+	s, _ = AppArguments.Get("logfile")
+	logStr := absolute(dataStr, s)
+	flag.StringVar(&logStr, "log", logStr,
+		"(optional) name of the logfile to write to\n")
+
+	/*
+		ndBool := false
+		flag.BoolVar(&ndBool, "nd", ndBool,
+			"(optional) no daemon: whether to not daemonise the program")
+	*/
+
+	portInt, _ := AppArguments.AsInt("port")
+	flag.IntVar(&portInt, "port", portInt,
+		"<portNumber> the IP port to listen to ")
+
+	realStr, _ := AppArguments.Get("realm")
+	flag.StringVar(&realStr, "realm", realStr,
+		"(optional) <hostName> name of host/domain to secure by BasicAuth\n")
+
+	themStr, _ := AppArguments.Get("theme")
+	flag.StringVar(&themStr, "theme", themStr,
+		"<name> the display theme to use ('light' or 'dark')\n")
+
+	uaStr := ""
+	flag.StringVar(&uaStr, "ua", uaStr,
+		"<userName> (optional) user add: add a username to the password file")
+
+	ucStr := ""
+	flag.StringVar(&ucStr, "uc", ucStr,
+		"<userName> (optional) user check: check a username in the password file")
+
+	udStr := ""
+	flag.StringVar(&udStr, "ud", udStr,
+		"<userName> (optional) user delete: remove a username from the password file")
+
+	s, _ = AppArguments.Get("passfile")
+	ufStr := absolute(dataStr, s)
+	flag.StringVar(&ufStr, "uf", ufStr,
+		"<fileName> (optional) user passwords file storing user/passwords for BasicAuth\n")
+
+	ulBool := false
+	flag.BoolVar(&ulBool, "ul", ulBool,
+		"(optional) user list: show all users in the password file")
+
+	uuStr := ""
+	flag.StringVar(&uuStr, "uu", uuStr,
+		"<userName> (optional) user update: update a username in the password file")
+
+	flag.Usage = ShowHelp
+	flag.Parse() // // // // // // // // // // // // // // // // // // //
+
+	AppArguments.set("booksperpage", fmt.Sprintf("%d", bppInt))
+
+	if 0 < len(dataStr) {
+		dataStr, _ = filepath.Abs(dataStr)
+	}
+	if f, err := os.Stat(dataStr); nil != err {
+		log.Fatalf("datadir == %s` problem: %v", dataStr, err)
+	} else if !f.IsDir() {
+		log.Fatalf("Error: Not a directory `%s`", dataStr)
+	}
+	AppArguments.set("datadir", dataStr)
+
+	if 0 < len(ckStr) {
+		ckStr = absolute(dataStr, ckStr)
+		if fi, err := os.Stat(ckStr); (nil != err) || (0 >= fi.Size()) {
+			ckStr = ""
+		}
+	}
+	AppArguments.set("certKey", ckStr)
+
+	if 0 < len(cpStr) {
+		cpStr = absolute(dataStr, cpStr)
+		if fi, err := os.Stat(cpStr); (nil != err) || (0 >= fi.Size()) {
+			cpStr = ""
+		}
+	}
+	AppArguments.set("certPem", cpStr)
+
+	/*
+		if 0 <len(intlStr) {
+			intlStr = absolute(dataStr, intlStr)
+		}
+		AppArguments.set("intl", intlStr)
+	*/
+
+	if 0 == len(langStr) {
+		langStr = "en"
+	}
+	AppArguments.set("lang", langStr)
+
+	if 0 == len(lnStr) {
+		lnStr = time.Now().Format("2006:01:02:15:04:05")
+	}
+	AppArguments.set("libraryname", lnStr)
+
+	AppArguments.set("librarypath", SetCalibreLibraryPath(lpStr))
+
+	if "0" == listenStr {
+		listenStr = ""
+	}
+	AppArguments.set("listen", listenStr)
+
+	if 0 < len(logStr) {
+		logStr = absolute(dataStr, logStr)
+	}
+	AppArguments.set("logfile", logStr)
+
+	/*
+		if ndBool {
+			s = "true"
+		} else {
+			s = ""
+		}
+		AppArguments.set("nd", s)
+	*/
+
+	AppArguments.set("port", fmt.Sprintf("%d", portInt))
+
+	AppArguments.set("realm", realStr)
+	AppArguments.set("theme", themStr)
+	AppArguments.set("ua", uaStr)
+	AppArguments.set("uc", ucStr)
+	AppArguments.set("ud", udStr)
+
+	if 0 < len(ufStr) {
+		ufStr = absolute(dataStr, ufStr)
+	}
+	AppArguments.set("uf", ufStr)
+
+	if ulBool {
+		s = "true"
+	} else {
+		s = ""
+	}
+	AppArguments.set("ul", s)
+
+	AppArguments.set("uu", uuStr)
+} // initArguments()
 
 // ShowHelp lists the commandline options to `Stderr`.
 func ShowHelp() {
