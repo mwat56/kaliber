3214a8b05114ae3cc95a14ada3be3b4a632d9668
Matthias Watermann <support@mwat.de>
2019-08-05 20:57:47 +0200

    * updated file;

--- queries_test.go
+++ queries_test.go
@@ -411,12 +411,12 @@ func TestQueryBy(t *testing.T) {
 		wantErr    bool
 	}{
 		// TODO: Add test cases.
-		{" 0", args{o0}, 5462, 1000, false},
+		{" 0", args{o0}, 5466, 1000, false},
 		{" 1", args{o1}, 14, 14, false},
-		{" 2", args{o2}, 4584, 50, false},
+		{" 2", args{o2}, 4585, 50, false},
 		{" 3", args{o3}, 42, 42, false},
-		{" 4", args{o4}, 358, 50, false},
-		{" 5", args{o5}, 448, 50, false},
+		{" 4", args{o4}, 359, 50, false},
+		{" 5", args{o5}, 449, 50, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -444,7 +444,7 @@ func TestQueryLimit(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
-		want    int // *TDocList
+		want    int
 		wantErr bool
 	}{
 		// TODO: Add test cases.
@@ -458,7 +458,7 @@ func TestQueryLimit(t *testing.T) {
 		{" 7", args{3500, 500}, 500, false},
 		{" 8", args{4000, 500}, 500, false},
 		{" 9", args{4500, 500}, 500, false},
-		{"10", args{5000, 500}, 462, false},
+		{"10", args{5000, 500}, 466, false},
 		{"11", args{5500, 500}, 0, false},
 	}
 	for _, tt := range tests {
