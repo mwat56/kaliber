7b2ad4740950572e0a35cac0a0752f28feaf887c
Matthias Watermann <support@mwat.de>
2019-08-28 14:28:10 +0200

    * major refactoring;

---
+++ metadata.go
@@ -0,0 +1,282 @@
+/*
+   Copyright © 2019 M.Watermann, 10247 Berlin, Germany
+                  All rights reserved
+               EMail : <support@mwat.de>
+*/
+
+package kaliber
+
+//lint:file-ignore ST1017 - I prefer Yoda conditions
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+
+	"github.com/mwat56/apachelogger"
+)
+
+const (
+	// Name of the JSON section holding e.g. user-defined field definitions.
+	mdFieldMetadata = "field_metadata"
+
+	// Name of the JSON section holding the virtual library definitions.
+	mdVirtualLibraries = "virtual_libraries"
+)
+
+type (
+	// Structure of the `virtual_libraries` JSON section.
+	tMdLibList map[string]string
+
+	// TmdVirtLibStruct is a structure to hold a virtual library definition.
+	TmdVirtLibStruct struct {
+		Def string // Calibre's definitions
+		SQL string // SQL: WHERE clause
+	}
+
+	// TvirtLibMap is a list of virt.lib. definitions
+	TvirtLibMap map[string]TmdVirtLibStruct
+)
+
+var (
+	// cache of "field_metadata" list
+	mdFieldsMetadata *map[string]interface{}
+
+	// cache of all DB metadata preferemces
+	mdMetadataDbPrefs *map[string]interface{}
+
+	// raw virtual libraries list
+	mdVirtLibsRaw *map[string]interface{}
+
+	// virtual libraries list
+	mdVirtLibList TvirtLibMap
+)
+
+// `mdReadMetadataFile()` returns a map of the JSON data read.
+func mdReadMetadataFile() error {
+	if nil != mdMetadataDbPrefs {
+		return nil // metadata already read
+	}
+	fName := CalibrePreferencesPath()
+	srcFile, err := os.OpenFile(fName, os.O_RDONLY, 0)
+	if nil != err {
+		msg := fmt.Sprintf("os.OpenFile(%s): %v", fName, err)
+		apachelogger.Log("md.mdReadMetadataFile", msg)
+		return errors.New(msg)
+	}
+	defer srcFile.Close()
+
+	var jsdata map[string]interface{}
+	dec := json.NewDecoder(srcFile)
+	if err := dec.Decode(&jsdata); err != nil {
+		msg := fmt.Sprintf("json.NewDecoder.Decode(): %v", err)
+		apachelogger.Log("md.mdReadMetadataFile", msg)
+		return errors.New(msg)
+	}
+
+	// remove unneeded list entries:
+	delete(jsdata, `books view split pane state`)
+	delete(jsdata, `column_icon_rules`)
+	delete(jsdata, `column_color_rules`)
+	delete(jsdata, `cover_grid_icon_rules`)
+	delete(jsdata, `gui_view_history`)
+	delete(jsdata, `namespaced:CountPagesPlugin:settings`)
+	delete(jsdata, `namespaced:FindDuplicatesPlugin:settings`)
+	delete(jsdata, `news_to_be_synced`)
+	delete(jsdata, `saved_searches`)
+	delete(jsdata, `update_all_last_mod_dates_on_start`)
+	delete(jsdata, `user_categories`)
+	mdMetadataDbPrefs = &jsdata
+
+	return nil
+} // mdReadMetadataFile()
+
+// `mdReadFieldMetadata()`
+func mdReadFieldMetadata() error {
+	if nil != mdFieldsMetadata {
+		return nil // field metadata already read
+	}
+	if err := mdReadMetadataFile(); nil != err {
+		msg := fmt.Sprintf("mdReadMetadataFile(): %v", err)
+		apachelogger.Log("md.mdReadFieldMetadata", msg)
+		return errors.New(msg)
+	}
+	section, ok := (*mdMetadataDbPrefs)[mdFieldMetadata]
+	if !ok {
+		msg := fmt.Sprintf("no such JSON section: %s", mdFieldMetadata)
+		apachelogger.Log("md.mdReadFieldMetadata", msg)
+		return errors.New(msg)
+	}
+	fmd := section.(map[string]interface{})
+	mdFieldsMetadata = &fmd
+
+	return nil
+} // mdReadFieldMetadata()
+
+// `mdGetFieldData()` returns a list of field definitions for `aField`.
+func mdGetFieldData(aField string) (map[string]interface{}, error) {
+	var result map[string]interface{}
+	if 0 == len(aField) {
+		return result, nil
+	}
+	if err := mdReadFieldMetadata(); nil != err {
+		msg := fmt.Sprintf("mdReadFieldMetadata(): %v", err)
+		apachelogger.Log("md.mdGetFieldData", msg)
+		return nil, errors.New(msg)
+	}
+
+	fmd := *mdFieldsMetadata
+	fd, ok := fmd[aField]
+	if !ok {
+		msg := fmt.Sprintf("no such JSON section: %s", aField)
+		apachelogger.Log("md.mdGetFieldData", msg)
+		return nil, errors.New(msg)
+	}
+	result = fd.(map[string]interface{})
+
+	return result, nil
+} // mdGetFieldData()
+
+// `mdGetFieldValue()`
+func mdGetFieldValue(aField, aKey string) (interface{}, error) {
+	var result interface{}
+	if (0 == len(aField)) || (0 == len(aKey)) {
+		return result, nil
+	}
+
+	fmd, err := mdGetFieldData(aField)
+	if nil != err {
+		msg := fmt.Sprintf("mdGetFieldData(): %v", err)
+		apachelogger.Log("md.mdGetFieldValue", msg)
+		return nil, errors.New(msg)
+	}
+
+	result, ok := fmd[aKey]
+	if !ok {
+		msg := fmt.Sprintf("no such JSON section: %s", aField)
+		apachelogger.Log("md.mdGetFieldValue", msg)
+		return nil, errors.New(msg)
+	}
+
+	return result, nil
+} // mdGetFieldValue()
+
+// `mdReadVirtLibs()` reads the raw virt.library definitions.
+func mdReadVirtLibs() error {
+	if nil != mdVirtLibsRaw {
+		return nil
+	}
+	if nil == mdMetadataDbPrefs {
+		if err := mdReadMetadataFile(); nil != err {
+			msg := fmt.Sprintf("mdReadMetadataFile(): %v", err)
+			apachelogger.Log("md.mdReadVirtLibs", msg)
+			return errors.New(msg)
+		}
+	}
+
+	section, ok := (*mdMetadataDbPrefs)[mdVirtualLibraries]
+	if !ok {
+		msg := fmt.Sprintf("no such JSON section: %s", mdVirtualLibraries)
+		apachelogger.Log("md.mdReadVirtLibs", msg)
+		return errors.New(msg)
+	}
+	vlr := section.(map[string]interface{})
+	mdVirtLibsRaw = &vlr
+
+	return nil
+} // mdReadVirtLibs()
+
+// `mdGetLibDefs()` returns a map of virtual library definitions.
+func mdGetLibDefs() (*tMdLibList, error) {
+	if nil == mdVirtLibsRaw {
+		if err := mdReadVirtLibs(); nil != err {
+			msg := fmt.Sprintf("mdReadVirtLibs(): %v", err)
+			apachelogger.Log("md.mdGetLibDefs", msg)
+			return nil, errors.New(msg)
+		}
+	}
+
+	m := *mdVirtLibsRaw
+	result := make(tMdLibList, len(m))
+	for key, value := range m {
+		if definition, ok := value.(string); ok {
+			result[key] = definition
+		} else {
+			msg := fmt.Sprintf("json.value.(string): wrong type %v", value)
+			apachelogger.Log("md.mdGetLibDefs", msg)
+		}
+	}
+
+	return &result, nil
+} // mdGetLibDefs()
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+var (
+	// RegEx to find `.*` in a virt.lib. definition
+	dotStarRE = regexp.MustCompile(`(\.?\*)`)
+)
+
+// GetVirtLibList returns a list of virtual library definitions
+// and SQL code to access them.
+func GetVirtLibList() (*TvirtLibMap, error) {
+	if nil != mdVirtLibList {
+		return &mdVirtLibList, nil
+	}
+	jsList, err := mdGetLibDefs()
+	if nil != err {
+		msg := fmt.Sprintf("mdGetLibDefs(): %v", err)
+		apachelogger.Log("md.GetVirtLibList", msg)
+		return nil, err
+	}
+	mdVirtLibList = make(TvirtLibMap, len(*jsList))
+	for key, value := range *jsList {
+		vl := NewSearch(value).Parse()
+		mdVirtLibList[key] = TmdVirtLibStruct{
+			Def: dotStarRE.ReplaceAllLiteralString(value, "%"),
+			SQL: vl.Where(),
+		}
+	}
+
+	return &mdVirtLibList, nil
+} // GetVirtLibList()
+
+// GetVirtLibOptions returns the SELECT/OPTIONs of virtual libraries.
+//
+//	aSelected Name of the currently selected library.
+func GetVirtLibOptions(aSelected string) string {
+	vlList, err := GetVirtLibList()
+	if nil != err {
+		msg := fmt.Sprintf("GetVirtLibList(): %v", err)
+		apachelogger.Log("md.GetVirtLibOptions", msg)
+		return ""
+	}
+
+	list := make([]string, 0, len(*vlList)+1)
+	if (0 == len(aSelected)) || ("-" == aSelected) {
+		list = append(list, `<option value="-" SELECTED> – </option>`)
+		aSelected = ""
+	} else {
+		list = append(list, `<option value="-"> – </option>`)
+	}
+	for key := range *vlList {
+		option := `<option value="` + key + `"`
+		if key == aSelected {
+			option += ` SELECTED`
+			aSelected = ""
+		}
+		option += `>` + key + `</option>`
+		list = append(list, option)
+	}
+	sort.Slice(list, func(i, j int) bool {
+		return strings.ToLower(list[i]) < strings.ToLower(list[j])
+	})
+
+	return strings.Join(list, "\n")
+} // GetVirtLibOptions
+
+/* _EoF_ */
---
+++ metadata_test.go
@@ -0,0 +1,217 @@
+/*
+   Copyright © 2019 M.Watermann, 10247 Berlin, Germany
+                  All rights reserved
+               EMail : <support@mwat.de>
+*/
+
+package kaliber
+
+//lint:file-ignore ST1017 - I prefer Yoda conditions
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_mdReadMetadataFile(t *testing.T) {
+	SetCalibreLibraryPath("/var/opt/Calibre")
+	var v1 tMdLibList
+	tests := []struct {
+		name    string
+		want    *tMdLibList
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{" 1", &v1, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := mdReadMetadataFile()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("mdReadMetadataFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if 0 == len(*mdMetadataDbPrefs) {
+				t.Errorf("mdReadMetadataFile() = %v, want %v", len(*mdMetadataDbPrefs), "> 0")
+			}
+		})
+	}
+} // Test_mdReadMetadataFile()
+
+func Test_mdGetLibDefs(t *testing.T) {
+	SetCalibreLibraryPath("/var/opt/Calibre")
+	tests := []struct {
+		name    string
+		want    *tMdLibList
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{" 1", nil, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := mdGetLibDefs()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("mdGetLibDefs() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if 0 == len(*got) {
+				t.Errorf("mdGetLibDefs() = %v, want %v", len(*got), "> 0")
+			}
+		})
+	}
+} // Test_mdGetLibDefs()
+
+func Test_GetVirtLibList(t *testing.T) {
+	SetCalibreLibraryPath("/var/opt/Calibre")
+	wl1 := &TvirtLibMap{}
+	tests := []struct {
+		name    string
+		want    *TvirtLibMap
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{" 1", wl1, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetVirtLibList()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetVirtLibList() error = %v,\nwantErr %v", err, tt.wantErr)
+				return
+			}
+			if 0 == len(*got) {
+				t.Errorf("GetVirtLibList() = %v, want %v", len(*got), "> 0")
+			}
+		})
+	}
+} // Test_GetVirtLibList()
+
+func TestGetVirtLibOptions(t *testing.T) {
+	SetCalibreLibraryPath("/var/opt/Calibre")
+	type args struct {
+		aSelected string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+		{" 1", args{""}},
+		{" 2", args{"Warentest"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetVirtLibOptions(tt.args.aSelected); 0 == len(got) {
+				t.Errorf("GetVirtLibOptions() = %v,\nwant %v", got, "> 0")
+			}
+		})
+	}
+} // TestGetVirtLibOptions()
+
+func Test_mdReadFieldMetadata(t *testing.T) {
+	SetCalibreLibraryPath("/var/opt/Calibre")
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{" 1", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := mdReadFieldMetadata(); (err != nil) != tt.wantErr {
+				t.Errorf("mdReadFieldMetadata() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if 0 == len(*mdFieldsMetadata) {
+				t.Errorf("GetVirtLibList() = %v, want %v", len(*mdFieldsMetadata), "> 0")
+			}
+		})
+	}
+} // Test_mdReadFieldMetadata()
+
+func Test_mdGetFieldData(t *testing.T) {
+	SetCalibreLibraryPath("/var/opt/Calibre")
+	var w1 map[string]interface{}
+	type args struct {
+		aKey string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[string]interface{}
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{" 1", args{"size"}, w1, false},
+		{" 2", args{"#genre"}, w1, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := mdGetFieldData(tt.args.aKey)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("mdGetFieldData() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			// if !reflect.DeepEqual(got, tt.want) {
+			// 	t.Errorf("mdGetFieldData() = %v, want %v", got, tt.want)
+			// }
+			if 0 == len(got) {
+				t.Errorf("mdGetFieldData() = %v, want %v", len(got), "> 0")
+			}
+		})
+	}
+} // Test_mdGetFieldData()
+
+func Test_mdReadVirtLibs(t *testing.T) {
+	SetCalibreLibraryPath("/var/opt/Calibre")
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{" 1", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := mdReadVirtLibs(); (err != nil) != tt.wantErr {
+				t.Errorf("mdReadVirtLibs() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if 0 == len(*mdVirtLibsRaw) {
+				t.Errorf("mdReadVirtLibs() = %v, want %v", len(*mdVirtLibsRaw), "> 0")
+			}
+		})
+	}
+} // Test_mdReadVirtLibs()
+
+func Test_mdGetFieldValue(t *testing.T) {
+	type args struct {
+		aField string
+		aKey   string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    interface{}
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{" 1", args{"authors", "is_category"}, true, false},
+		{" 2", args{"authors", "table"}, "authors", false},
+		{" 3", args{"#genre", "is_category"}, true, false},
+		{" 4", args{"#genre", "is_custom"}, true, false},
+		{" 5", args{"#genre", "table"}, "custom_column_1", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := mdGetFieldValue(tt.args.aField, tt.args.aKey)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("mdGetFieldValue() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mdGetFieldValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+} // Test_mdGetFieldValue()
